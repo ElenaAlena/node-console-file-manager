60d91dca5fe5c5b7e33d8eeae2aef53367e5b7cf
ElenaAlena <elena.pigaleva@gmail.com>
2022-12-11 14:54:30 +0300

    Hash calculation

--- src/modules/hash/hash.js
+++ src/modules/hash/hash.js
@@ -1,3 +1,18 @@
-export const hash = async (...params) => {
-    console.log(`hash: ${params.join(" ,")}`);
-  };
+import { readFile } from "fs/promises";
+import { createHash } from "crypto";
+import { DEFAULT_ERRORS } from "../../helpers/constants.js";
+import { normalizePath } from "../../helpers/utils.js";
+
+export const hash = async (currentPath, ...params) => {
+  const [pathToFile] = params;
+  if (!pathToFile || params.length > 1) throw DEFAULT_ERRORS.invalidInput;
+  try {
+    const filePath = normalizePath(currentPath, pathToFile);
+    const data = await readFile(filePath);
+    const result = createHash("sha256").update(data).digest("hex");
+    console.log(result);
+    return { path: currentPath };
+  } catch {
+    throw DEFAULT_ERRORS.operationFailed;
+  }
+};
